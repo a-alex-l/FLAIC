bb003b0dbf7071160a21a93f0707d9b7b2207e66
A_Alex_L <lesnik-a-a@yandex.ru>
2025-07-29 15:44:02 +0300

    Typo fixed plus extra code and comments

--- .eleventy.js
+++ .eleventy.js
@@ -1,33 +1,22 @@
 module.exports = function(eleventyConfig) {
-  // Копировать ассеты
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("api");
 
-  // Указываем, что .html файлы должны обрабатываться как Nunjucks
   eleventyConfig.addExtension("html", {
-    key: "njk", // Используем 'njk' как расширение для обработки HTML
+    key: "njk", 
     engine: "html",
-    // Здесь можно добавить другие опции Nunjucks, если понадобятся
   });
 
-  // Указываем, что по умолчанию Nunjucks используется для всех файлов без явного Front Matter
-  // Это не всегда нужно, но часто помогает
-  // eleventyConfig.setTemplateFormats(["html", "njk", "md"]);
-
-  // Или, что более просто и часто работает:
   eleventyConfig.addGlobalData("eleventyComputed.permalink", function() {
     return (data) => `${data.page.filePathStem}.html`;
   });
 
-
-  // Возвращаем конфигурацию
   return {
     dir: {
       input: ".",
       includes: "_includes",
       output: "_site",
     },
-    // Указываем, что по умолчанию HTML-файлы обрабатываются как Nunjucks
     htmlTemplateEngine: "njk", 
     dataTemplateEngine: "njk" 
   };
